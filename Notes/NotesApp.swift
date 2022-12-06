0139b0fb788e999e24f4153452c8e85c547dea38
Yasin Şükrü TAN <yasinsukrutan@gmail.com>
2022-12-06 12:53:33 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Yasin Şükrü Tan on 6.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
